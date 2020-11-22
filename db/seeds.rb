423061c58926ce34aee390c2b9a725fe56abc35d
bminczu <bminczu@gmail.com>
2020-11-22 14:49:37 -0600

    commit tree 5775b5808beaf00e9b846068eefa6325c3014ef2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+coupon1 = Coupon.create(coupon_code :"34534", store:"TB")
+coupon2 = COupon.create(coupon_code :"34532", store:"whatever")
